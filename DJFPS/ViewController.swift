709f1f79299519b2aa7d4fb679ee958b1a261978
jiqinqiang <jiqinqiang@lukou.com>
2018-02-26 16:58:36 +0800

    init

--- DJFPS/ViewController.swift
+++ DJFPS/ViewController.swift
@@ -12,12 +12,9 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+    
+    let label = DJFPSLabel(frame: CGRect(x: 100, y: 100, width: 55, height: 22))
+    view.addSubview(label)
   }
 
 
